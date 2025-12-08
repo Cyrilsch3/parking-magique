473fc059afe3f2b8a2970ae04c93b9b4b414b7de
Usnaamer <gaspard.munguiaxica2004@gmail.com>
2025-12-08 20:55:14 +0100

    1  commit db

--- Creat_Db.py
+++ Creat_Db.py
@@ -1,1 +1,15 @@
+import sqlite3
+import os
+import sys
+import traceback
+
+fichier_db = "parking.db"
+
+
+if os.path.exists(fichier_db):
+    print(f"La base '{fichier_db}' existe déjà. Connexion en cour...")
+else:
+    print(f"La base de donnée '{fichier_db}' n'existe pas. Création en cour...")
+    
+    
 
