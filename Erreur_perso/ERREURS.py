2fb408aae3856d08bcef59f51222789a0bb71705
SofianeAmqrane <sofiane.amqrane@icloud.com>
2025-12-19 12:28:28 +0100

    Update ERREURS.py

--- Erreur_perso/ERREURS.py
+++ Erreur_perso/ERREURS.py
@@ -2,6 +2,10 @@ class PlaceOccupeeError(Exception):
     # La place est déjà occupée
     pass
 
+class PlaceInvalideException(Exception):
+    # La place entré n'existe pas / n'est pas valide
+    pass
+
 #ici tu mets ton exception puis 
 # tu remplaces le value error que tu veux dans les classes 
 # et apres tu vas dans GUI et tu changes aussi le value error par le nom de ton exception
